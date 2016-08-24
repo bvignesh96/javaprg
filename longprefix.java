14ed803b584c249cf5142d601f2ed470ed0ff9c7
bvignesh96 <bvignesh2196@gmail.com>
2016-08-24 17:51:01 +0530

    Create longprefix.java

---
+++ longprefix.java
@@ -0,0 +1,29 @@
+class longprefix
+{
+public static void main(String[] args)
+{
+scanner sc=new Scanner(System.in);
+system.out.prinln("enter the 10 string:");
+String[] x=new String[10];
+int maxlen=0;
+for(int i=0;i<10;i++)
+  {
+  x[i]=sc.next();
+  if(x[i].length()>maxlen)
+    maxlen=i;
+  }
+  String s=x[maxlen];
+  int c=0;
+  for(int i=0;i<s.length();i++)
+  {
+    for(int j=0;j<x.length;j++)
+    {
+    if(s.charAt(i)==x[j].charAt(i) && s!=x[j])
+      {
+        c+=1;
+      }
+    }
+  }
+  System.out.println("long prefix is "+s.substring(0,c+1));
+}
+}
