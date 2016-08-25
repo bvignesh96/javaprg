7fecad1cc404db4d9b2e969a8837b7ef1b28720c
bvignesh96 <bvignesh2196@gmail.com>
2016-08-25 09:43:19 +0530

    Create firstrep.java

---
+++ firstrep.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class firstrep
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter array size:");
+int s=sc.nextInt();
+int[] arr=new int[s];
+System.out.println("enter elements:");
+for(int i=0;i<s;i++)
+  arr[i]=sc.nextInt();
+for(int i=0;i<s;i++)
+  for(int j=i+1;i<s;j++)
+      if(arr[i]==arr[j])
+      {
+      System.out.println("first repeated number is:"+arr[i]);
+      break;
+      }
+}
+}
