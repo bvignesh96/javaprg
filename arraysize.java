65439c97dc6ba9cccfdceaecccfd623d499633f2
bvignesh96 <bvignesh2196@gmail.com>
2016-08-28 10:59:33 +0530

    Create arraysize.java

---
+++ arraysize.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class arraysize
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the array size:");
+int s=sc.nextInt();
+int[] arr=new int[s];
+for(int i=0;i<s;i++)
+  arr[i]=sc.nextInt();
+for(int i=0;i<s;i++)
+{
+int c=0;
+for(int j=0;j<s;j++)
+{
+  if(arr[i]==arr[j])
+    c+=1;
+}
+if(c!=2)
+    System.out.println(arr[i]+" is single");
+}
+}
