027a2a0f616cbc4d6580a0fecc19d76d2ac5f384
bvignesh96 <bvignesh2196@gmail.com>
2016-08-28 10:45:27 +0530

    Update square.java

--- square.java
+++ square.java
@@ -3,12 +3,12 @@ class square
 {
 public static void main(String[] args)
 {
-Scanner sc=new Scanner(Sytem.in);
+  Scanner sc=new Scanner(System.in);
   System.out.println("Enter the points for square:");
   int[] arr=new int[8];
   for(int i=0;i<8;i++)
     arr[i]=sc.nextInt();
-    if((arr[3]-arr[1])==(arr[5]-arr[7])==(arr[4]-arr[2])==(arr[6]-arr[0]))
+    if(((arr[3]-arr[1])-(arr[5]-arr[7]))== 0 && ((arr[4]-arr[2])-(arr[6]-arr[0]))==0)
         System.out.println("Square");
     else
         System.out.println("Not a square");
